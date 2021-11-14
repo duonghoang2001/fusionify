180acf4b0bcc906f35f316d1fdfd8c5930b6ea48
Laura Reese <laurareese03@gmail.com>
2021-11-14 05:48:02 -0500

    added final axios calls and generate new playlist with as many songs from both users as possible

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button, Checkbox } from '@material-ui/core';
+import { Button, TextField, createTheme, ThemeProvider } from '@material-ui/core';
 
-import { getTrack, getTopArtists, getCurrentUser, createNewPlaylist } from './functions.js';
-
-import { createTheme, ThemeProvider } from '@material-ui/core';
+import { getCurrentUser, createNewPlaylist, getExternalUser, getUserPlaylists, getPlaylistTracks, addTracksToPlaylist } from './functions.js';
 
 const theme = createTheme({
   palette: {
@@ -14,7 +12,10 @@ const theme = createTheme({
     secondary: {
       main: "#1db954"
     }
-  }
+  },
+  typography: {
+    textAlign: 'center'
+  },
 });
 
 class App extends Component {
@@ -28,11 +29,18 @@ class App extends Component {
       auth: {headers: {'Authorization': 'Bearer ' + token}},
       track: null,
       currentUser: null,
+      currentUserPlaylists: [],
+      oppUserID: '',
+      oppUser: null,
+      oppUserPlaylists: [],
+      oppUserIDValidated: false,
+      tracks: [],
+      tracks2: [],
+      newPlaylist: null,
     }
   }
 
   componentDidMount() {
-    this.getFriends();
     this.getCurrentUser();
   }
 
@@ -48,48 +56,104 @@ class App extends Component {
     return hashParams;
   }
 
-  listUsers() {
-    return <Checkbox />;
-  }
-
-  getFriends(){
-    getTrack('11dFghVXANMlKmJXsNCbNl', this.state.auth).then(track => {
-      this.setState({ track: track }, () => {
-        console.log(this.state.track.data);
-      });
-    });
-  }
-
   getCurrentUser(){
     getCurrentUser(this.state.auth).then(user => {
-      this.setState({ currentUser: user }, () => {
+      this.setState({ currentUser: user.data }, () => {
         console.log(this.state.currentUser);
       });
     });
   }
 
+  getExternalUser(){
+    getExternalUser(this.state.oppUserID, this.state.auth).then(user => {
+      if(user != null) {
+        this.setState({ oppUser: user.data }, () => {
+          console.log(this.state.oppUser);
+        });
+      }
+    });
+  }
+
+  handleTextFieldChange = (event) => {
+    this.setState({ oppUserID: event.target.value });
+  }
+
   createNewPlaylist(){
-    createNewPlaylist(this.state.currentUser.data.id, "test playlist", this.state.auth);
+    createNewPlaylist(this.state.currentUser.id, "test playlist", this.state.auth).then(playlist => {
+      this.setState({ newPlaylist: playlist.data });
+    });
+
+    getUserPlaylists(this.state.currentUser.id, this.state.auth).then(playlists => {
+      this.setState({ currentUserPlaylists: playlists.data.items }, () => {
+        this.state.currentUserPlaylists.map((playlist, index) => {
+          getPlaylistTracks(playlist.id, this.state.auth).then(tracks => {
+            this.setState({ tracks: tracks.data }, () => {
+              console.log(tracks.data, ' 86');
+              this.trackObjectToURIs();
+            })
+          });
+        });
+      });
+    });
+
+    getUserPlaylists(this.state.oppUserID, this.state.auth).then(playlists => {
+      this.setState({ oppUserPlaylists: playlists.data.items }, () => {
+        this.state.oppUserPlaylists.map((playlist, index) => {
+          getPlaylistTracks(playlist.id, this.state.auth).then(tracks => {
+            this.setState({ tracks2: tracks.data }, () => {
+              console.log(tracks, ' 102');
+              this.trackObjectToURIs2();
+            })
+          });
+        });
+      });
+    });
+  }
+
+  trackObjectToURIs() {
+    const uriTracks = this.state.tracks.items.map((track) => track.track.uri);
+    console.log(uriTracks, " 114");
+    this.setState({ tracks: uriTracks }, () => {
+      addTracksToPlaylist(this.state.newPlaylist.id, this.state.tracks, this.state.auth).then(tracks => {
+        console.log(tracks);
+      });
+    });
+  }
+
+  trackObjectToURIs2() {
+    const uriTracks = this.state.tracks2.items.map((track) => track.track.uri);
+    console.log(uriTracks, " 114");
+    this.setState({ tracks2: uriTracks }, () => {
+      addTracksToPlaylist(this.state.newPlaylist.id, this.state.tracks2, this.state.auth).then(tracks => {
+        console.log(tracks);
+      });
+    });
   }
 
   render() {
     return (
       <div className="App">
         {this.state.track && <div>
-          {this.state.track.data.name}
+          {this.state.track.name}
         </div>}
         {this.state.currentUser && <div>
-          {this.state.currentUser.data.display_name}
+          {this.state.currentUser.display_name}
         </div>}
         {!this.state.loggedIn && <a href='http://localhost:8888' > Login to Spotify </a>}
 
         { this.state.loggedIn &&
           <ThemeProvider theme={theme}>
+            <div textAlign='center'>
+              <TextField required focused disabled={this.state.oppUserIDValidated} id='Opposite User' variant='filled' label='User to match' value={this.state.oppUserID} onChange={this.handleTextFieldChange} />
+              <Button variant='contained' color='primary' onClick={() => this.getExternalUser()}>
+                Find User
+              </Button>
+            </div>
+            {this.state.oppUser && <div>{this.state.oppUser.display_name}</div>}
             <div>
               <Button variant='contained' color='primary' onClick={() => this.createNewPlaylist()}>
                 Generate Playlist
               </Button>
-              <Checkbox />
             </div>
             {/* <div>
               <Button variant='contained' color='primary' onClick={() => this.getNowPlaying()}>
--- client/src/functions.js
+++ client/src/functions.js
@@ -7,7 +7,7 @@ export function getTrack(trackID, auth) {
     axios.get(baseURL + 'tracks/' + trackID, auth).then(res => {
       resolve(res);
     }).catch(error => {
-      reject(error);
+      reject(error ? error : "Cannot get track information");
     });
   });
 }
@@ -17,7 +17,7 @@ export function getTopArtists(auth) {
     axios.get(baseURL + 'me/top/artists', auth).then(res => {
       resolve(res);
     }).catch(error => {
-      reject(error);
+      reject(error ? error : "Cannot get top artists");
     });
   });
 }
@@ -27,7 +27,17 @@ export function getCurrentUser(auth) {
     axios.get(baseURL + 'me', auth).then(res => {
       resolve(res);
     }).catch(error => {
-      reject(error);
+      reject(error ? error : "Cannot get current user");
+    });
+  });
+}
+
+export function getExternalUser(userID, auth) {
+  return new Promise(function(resolve, reject) {
+    axios.get(baseURL + 'users/' + userID, auth).then(res => {
+      resolve(res);
+    }).catch(error => {
+      reject(error ? error : "Unable to find user");
     });
   });
 }
@@ -40,6 +50,38 @@ export function createNewPlaylist(userID, playlistName, auth) {
       resolve(res);
     }).catch(error => {
       reject(error ? error : "Unable to create playlist");
+    });
+  });
+}
+
+export function getUserPlaylists(userID, auth) {
+  return new Promise(function(resolve, reject) {
+    axios.get(baseURL + 'users/' + userID + '/playlists', auth).then(res => {
+      resolve(res);
+    }).catch(error => {
+      reject(error ? error : "Unable to find user playlists");
+    });
+  });
+}
+
+export function getPlaylistTracks(playlistID, auth) {
+  return new Promise(function(resolve, reject) {
+    axios.get(baseURL + 'playlists/' + playlistID + '/tracks', auth).then(res => {
+      resolve(res);
+    }).catch(error => {
+      reject(error ? error : "Unable to load playlist tracks");
+    });
+  });
+}
+
+export function addTracksToPlaylist(playlistID, tracks, auth) {
+  return new Promise(function(resolve, reject) {
+    axios.post(baseURL + 'playlists/' + playlistID + '/tracks', {
+      "uris": tracks
+    }, auth).then(res => {
+      resolve(res);
+    }).catch(error => {
+      reject(error ? error : "Unable to add tracks to playlist");
     });
   });
 }
